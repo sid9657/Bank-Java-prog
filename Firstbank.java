366db9c4e644284dc8a29e79e8161706f264095e
Siddhesh Deokar <145990705+sid9657@users.noreply.github.com>
2024-08-28 21:50:49 +0530

    Create Firstbank.java prog
    update

---
+++ Firstbank.java
@@ -0,0 +1,12 @@
+package bank;
+
+public class FirstBank {
+    public static void main(String[] args) {
+        Account newAcc=new Account("Sanket", "ac001");
+        Account newAcc2=new Account("viplav", "ac220");
+    newAcc2.showMenu();
+
+    }
+    
+    
+}
